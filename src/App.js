4ee5c62374ea6e286d0db3646da6e8c79fe7af1a
coolb0y <57170752+coolb0y@users.noreply.github.com>
2023-07-07 21:07:01 +0530

    changes regarding logo placement and few options added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
   ResultList,
   SingleList
 } from "@appbaseio/reactivesearch";
+import "./styles.css";
 
 import { useState } from "react";
 
@@ -16,7 +17,7 @@ function App() {
   const indexname = process.env.REACT_APP_INDEX_NAME;
   const [fuzzinessval,setFuzzinessval]=useState(2);
   const [queryFormatval,setQueryFormatval]=useState("or");
-  const [exactMatch, setExactMatch] = useState(false);
+  const [allMatch, setAllMatch] = useState(false);
   const [phraseMatch, setPhraseMatch] = useState(false);
 
   function generateLinkString(url) {
@@ -27,16 +28,16 @@ function App() {
     return formattedURL;
   }
 
-  const handleExactMatchChange = (event) => {
-    setExactMatch(event.target.checked);
-    console.log(exactMatch)
-    if(exactMatch){
-      console.log(exactMatch,'exactMatch fn')
+  const handleAllMatchChange = (event) => {
+    setAllMatch(event.target.checked);
+    console.log(allMatch)
+    if(allMatch){
+      console.log(allMatch,'All Match fn')
       setFuzzinessval(2);
       setQueryFormatval("or");
     }
     else{
-      console.log(exactMatch,'exactMatch fn1')
+      console.log(allMatch,'All Match fn1')
         setFuzzinessval(0);
         setQueryFormatval("and");
     }
@@ -145,10 +146,14 @@ function App() {
 
   return (
 
+   
+    <div style={{textAlign:"left",marginTop:"20px",marginBottom:'0' ,display:"inline-block" ,flex:"1"} }>
     
+    
+  
     <ReactiveBase
       url="http://admin:admin@127.0.0.1:9200"
-      app={indexname?indexname:"test5"}
+      app={indexname?indexname:"chipsterindex"}
       credentials="admin:admin"
       enableAppbase={false}
       
@@ -158,87 +163,62 @@ function App() {
 			// }}
     >
       {/* other components will go here. */}
-      
+     
       <div style={{ display: "flex", flexDirection: "row" }}>
+     
         <div
           style={{
             display: "block",
             flexDirection: "column",
             width: "30%",
-            margin: "10px",
-            maxHeight: '600px',
+            margin: "15px",
+            maxHeight: '700px',
             maxWidth: '350px',
             overflowY: 'scroll',
-            marginTop: '100px',
-            
-            
+            marginTop: '30px',
+           
+            paddingRight: '15px',
           }}
-        >   
+          >   
+         <img src={process.env.PUBLIC_URL + '/images/SearchPageLogo.png'} alt="Chipster" width="350rem" />
 
+         <div>
 
-        <div>
-
-        <label htmlFor="exactMatch" style={{ fontWeight: "bold" ,display:"block" }}>Matching all Terms</label>
-            <input
-            type="checkbox"
-            checked={exactMatch}
-            style={{
-              width: "19px",
-              height: "19px",
-              marginLeft: "1px",
-              marginRight: "25px",
-              display:"inline"
-            
-          }}
-            onChange={handleExactMatchChange}
-          />
-
-           <label htmlFor="exactMatch" style={{ display:"inline" , marginBottom:"2px" }}>Check for All term Match</label>
-          
-
-
-           <label htmlFor="PhraseMatch" style={{ fontWeight: "bold" ,display:"block" }}>Phrase Match</label>
-            <input
-            type="checkbox"
-            checked={phraseMatch}
-            style={{
-              width: "19px",
-              height: "19px",
-              marginLeft: "1px",
-              marginRight: "25px",
-              display:"inline"
-            
-          }}
-            onChange={handlePhraseMatchChange}
-          />
-           <label htmlFor="exactMatch" style={{ display:"inline" , marginBottom:"2px" }}>Check for Phrase Matching</label>
-          
          
           <MultiList
             showSearch={true}
             componentId="filetypefilter"
             dataField="filetype"
-            title="Filter by file type"
-            fielddata={true}
+            title="Filter by File Type"
+            //fielddata={false}
             size={5}
-            
+            react={{
+              and: ["searchbox", "sizefilter","baseurlfilter","filetypefilter"]
+            }}
           />
 
           <MultiList
             showSearch={true}
             componentId="baseurlfilter"
             dataField="baseurl"
-            title="Filter by Baseurl"
+            title="Filter by Website"
             fielddata={true}
             size={5}
             
+            react={{
+              and: ["searchbox", "sizefilter","baseurlfilter","filetypefilter"]
+            }}
           />
           
-          
+
+      
           <SingleRange
             componentId="sizefilter"
             dataField="filesize"
             title="Filter by File Size"
+            react={{
+              and: ["searchbox", "sizefilter","baseurlfilter","filetypefilter"]
+            }}
             data={[
               { start: 1000000, end: 2000000, label: "1Mb to 2Mb" },
               { start: 2000001, end: 5000000, label: "2Mb to 5Mb" },
@@ -249,25 +229,6 @@ function App() {
             defaultValue=""
           />
  
-
-            {/* <SingleList
-            componentId="baseurlfilter"
-            dataField="baseurl"
-            title="Base URL"
-            size={6}
-            //selectAllLabel="All Cities"
-           // showRadio={true}
-            showCount={true}
-           // showSearch={true}
-            placeholder="Search URL"
-            // react={{
-            //   and: ["CategoryFilter", "SearchFilter"]
-            // }}
-            showFilter={true}
-            filterLabel="Base URL"
-            URLParams={false}
-            loader="Loading ..."
-          /> */}
          </div>
          
          </div>
@@ -275,7 +236,9 @@ function App() {
         <div style={{ display: "flex", flexDirection: "column", width: "66%" }}>
           <DataSearch
             style={{
-              marginTop: "35px"
+              marginTop: "35px",
+              boxShadow: "none"
+              
             }}
           debounce={500}
           showVoiceSearch={true}
@@ -293,9 +256,55 @@ function App() {
               "album",
               "imgtags"
             ]}
+            react={{
+              and: ["searchbox", "sizefilter","baseurlfilter","filetypefilter"]
+            }}
             fieldWeights={[7, 4, 3,2,1 ]}
-            placeholder="Search for Learning.."
+            placeholder="Search.."
+           />
+
+
+
+
+             <div >
+             <input
+            type="checkbox"
+            checked={allMatch}
+            style={{
+              width: "19px",
+              height: "19px",
+              marginLeft: "1px",
+              marginRight: "5px",
+              display:"inline"
+            
+          }}
+            onChange={handleAllMatchChange}
           />
+           <label htmlFor="allMatch" style={{ display:"inline" , marginBottom:"2px" ,marginRight:'10px' }}>Match All Terms</label>
+          
+
+          
+            <input
+            type="checkbox"
+            checked={phraseMatch}
+            style={{
+              width: "19px",
+              height: "19px",
+              marginLeft: "1px",
+              marginRight: "5px",
+              display:"inline"
+            
+          }}
+            onChange={handlePhraseMatchChange}
+          />
+
+           <label htmlFor="phraseMatch" style={{ display:"inline" , marginBottom:"2px" }}>Match Full Phrase</label>
+
+          <a href="www.google.com" style={{display:"inline", textDecoration:"none" ,paddingLeft: "30rem",color:"#3ea9e6"}}><h5 style={{display:"inline"}}>Search Help</h5></a>
+          <a href="www.google.com" style={{display:"inline", textDecoration:"none",paddingLeft: "40px",color:"#3ea9e6",marginRight:"0"}}><h5 style={{display:"inline"}}>Advanced Search</h5></a>
+           </div>
+
+
           <ReactiveList
             componentId="results"
             dataField="title"
@@ -348,10 +357,10 @@ function App() {
                         }}
                       /> */}
                        <ResultList.Content>
-                        <a href={item.url} style={{ textDecoration: "none", color: "blue" }}>
+                        <a href={item.url} style={{ textDecoration: "none", color: "#3ea9e6" }}>
                       
                       <ResultList.Title
-                      style={{ textDecoration: "none", color: "blue" }}
+                      style={{ textDecoration: "none", color: "#3ea9e6" }}
                         dangerouslySetInnerHTML={{
                           __html: item.title?item.title:"No Title Found"
                         }}
@@ -372,6 +381,7 @@ function App() {
         </div>
       </div>
     </ReactiveBase>
+    </div>
   );
 }
 
