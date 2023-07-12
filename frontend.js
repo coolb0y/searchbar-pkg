fb20dfbcd72a680102609de94cbd79debc06a34a
coolb0y <57170752+coolb0y@users.noreply.github.com>
2023-07-12 17:56:46 +0530

    including new build folder

--- frontend.js
+++ frontend.js
@@ -5,11 +5,12 @@ const app = express();
 path.join(__dirname, 'build/images/SearchPageLogo.png');
 path.join(__dirname, 'build/index.html');
 path.join(__dirname, 'build/asset-manifest.json');
-path.join(__dirname, 'build/static/js/main.94da1dd8.js');
-path.join(__dirname, 'build/static/js/main.94da1dd8.js.map');
-path.join(__dirname, 'build/static/js/main.94da1dd8.js.LICENSE.txt');
-path.join(__dirname, 'build/static/css/main.253dbbb2.css');
-path.join(__dirname, 'build/static/css/main.253dbbb2.css.map');
+path.join(__dirname, 'build/static/js/main.91af5311.js');
+path.join(__dirname, 'build/static/js/main.91af5311.js.map');
+path.join(__dirname, 'build/static/js/main.91af5311.js.LICENSE.txt');
+path.join(__dirname, 'build/static/css/main.16351879.css');
+path.join(__dirname, 'build/static/css/main.16351879.css.map');
+path.join(__dirname, 'build/favicon.png');
 
 app.use(express.static(path.join(__dirname, 'build')));
 
