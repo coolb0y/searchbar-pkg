527f94ac679c25943ae8cf5d729a8b33ba793114
coolb0y <57170752+coolb0y@users.noreply.github.com>
2023-12-21 01:01:28 +0530

    frontend change

--- frontend.js
+++ frontend.js
@@ -5,11 +5,11 @@ const app = express();
 path.join(__dirname, 'build/images/SearchPageLogo.png');
 path.join(__dirname, 'build/index.html');
 path.join(__dirname, 'build/asset-manifest.json');
-path.join(__dirname, 'build/static/js/main.91af5311.js');
-path.join(__dirname, 'build/static/js/main.91af5311.js.map');
-path.join(__dirname, 'build/static/js/main.91af5311.js.LICENSE.txt');
-path.join(__dirname, 'build/static/css/main.16351879.css');
-path.join(__dirname, 'build/static/css/main.16351879.css.map');
+path.join(__dirname, 'build/static/js/main.bfa8acca.js');
+path.join(__dirname, 'build/static/js/main.bfa8acca.js.map');
+path.join(__dirname, 'build/static/js/main.bfa8acca.js.LICENSE.txt');
+path.join(__dirname, 'build/static/css/main.1a0ba00f.css');
+path.join(__dirname, 'build/static/css/main.1a0ba00f.css.map');
 path.join(__dirname, 'build/favicon.png');
 
 app.use(express.static(path.join(__dirname, 'build')));
