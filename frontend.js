74fd03d56e5aee809b10a46b6801fba7d8d110ef
coolb0y <57170752+coolb0y@users.noreply.github.com>
2024-03-01 00:10:50 +0530

    checkbox fixed

--- frontend.js
+++ frontend.js
@@ -5,11 +5,11 @@ const app = express();
 path.join(__dirname, 'build/images/SearchPageLogo.png');
 path.join(__dirname, 'build/index.html');
 path.join(__dirname, 'build/asset-manifest.json');
-path.join(__dirname, 'build/static/js/main.65be46fa.js');
-path.join(__dirname, 'build/static/js/main.65be46fa.js.map');
-path.join(__dirname, 'build/static/js/main.65be46fa.js.LICENSE.txt');
-path.join(__dirname, 'build/static/css/main.a8a7f765.css');
-path.join(__dirname, 'build/static/css/main.a8a7f765.css.map');
+path.join(__dirname, 'build/static/js/main.a2cd0160.js');
+path.join(__dirname, 'build/static/js/main.a2cd0160.js.map');
+path.join(__dirname, 'build/static/js/main.a2cd0160.js.LICENSE.txt');
+path.join(__dirname, 'build/static/css/main.f3d684b1.css');
+path.join(__dirname, 'build/static/css/main.f3d684b1.css.map');
 path.join(__dirname, 'build/favicon.png');
 
 app.use(express.static(path.join(__dirname, 'build')));
